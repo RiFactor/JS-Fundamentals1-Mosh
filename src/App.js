b4277416e5a3f8893d794e1533e55d291c5dbedf
ri-factor <ri@3sidedcube.com>
2023-01-16 17:20:54 +0000

    JS1 - Objects

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+
+// Objects
 import ObjEnumerateProps from './pages/JS1/Objects/enumeratingproperties';
 import ExerciseAddressObject from './pages/JS1/Objects/exerciseaddressobject';
 import ExerciseFactoryAndConstructorFunctions from './pages/JS1/Objects/exerciseFactoryAndConstructorFunctions';
 import ExerciseObjectEquality from './pages/JS1/Objects/exerciseObjectEquality';
 import ExerciseBlogPostObject from './pages/JS1/Objects/exerciseBlogPostObject';
 import ExerciseConstructorFunctions from './pages/JS1/Objects/exerciseConstructorFunctions';
+import ExercisePriceRangeObject from './pages/JS1/Objects/exercisePriceRangeObject';
+
+//Arrays
+import Elements from './pages/JS1/Arrays/elements';
+import ShallowCopy from './pages/JS1/Arrays/shallowCopy';
 
 function App() {
   return (
     <>
-      <ObjEnumerateProps/>
-      <ExerciseAddressObject/>
-      <ExerciseFactoryAndConstructorFunctions/>
-      <ExerciseObjectEquality/>
-      <ExerciseBlogPostObject/>
-      <ExerciseConstructorFunctions/>
+      <section className='Objects'>
+        <ObjEnumerateProps/>
+        <ExerciseAddressObject/>
+        <ExerciseFactoryAndConstructorFunctions/>
+        <ExerciseObjectEquality/>
+        <ExerciseBlogPostObject/>
+        <ExerciseConstructorFunctions/>
+        <ExercisePriceRangeObject/>
+      </section>
+      <section className='Arrays'>
+        <Elements/>
+        <ShallowCopy/>
+      </section>
       </>
   );
 }
---
+++ src/pages/JS1/Objects/exercisePriceRangeObject.jsx
@@ -0,0 +1,26 @@
+const ExercisePriceRangeObject = () => {
+  let priceRanges = [
+    {
+      icon: "$",
+      tooltip: "Inexpensive",
+      minPricePerPerson: 0,
+      maxPricePerPeson: 15,
+    },
+    {
+      icon: "$$",
+      tooltip: "Moderate",
+      minPricePerPerson: 16,
+      maxPricePerPeson: 25,
+    },
+    {
+      icon: "$$$",
+      tooltip: "Expensive",
+      minPricePerPerson: 25,
+      maxPricePerPeson: 50,
+    },
+  ];
+
+  let restaurants = [{ averageMealPricePerPerson: 5 }];
+};
+
+export default ExercisePriceRangeObject;
